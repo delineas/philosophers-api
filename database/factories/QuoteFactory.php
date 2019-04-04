187d7c47aa9cb56ebae75d0f38878655b202bdb2
Daniel Primo <delineas@gmail.com>
2019-04-04 22:03:33 +0200

    🔧 Add migrations

---
+++ database/factories/QuoteFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Quote::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
--- database/migrations/2019_04_04_195639_create_currents_table.php
+++ database/migrations/2019_04_04_195639_create_currents_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAuthorsTable extends Migration
+class CreateCurrentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateAuthorsTable extends Migration
      */
     public function up()
     {
-        Schema::create('authors', function (Blueprint $table) {
+        Schema::create('currents', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('gender');
-            $table->string('life');
-            $table->string('image');
-            $table->string('history');
+            $table->unsignedInteger('author_id');
+            $table->string('current');
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateAuthorsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('authors');
+        Schema::dropIfExists('currents');
     }
 }
