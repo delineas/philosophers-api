6b1ede1d6a62595a770af92a7f7234be65827247
Daniel Primo <delineas@gmail.com>
2019-04-07 22:51:22 +0200

    🚧 Add Resources

---
+++ app/Http/Resources/Author.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Author extends JsonResource
+{
+    /**
+     * Indicates if the resource's collection keys should be preserved.
+     *
+     * @var bool
+     */
+    public $preserveKeys = true;
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Resources\Author as AuthorResource;
+use App\Author;
+use App\Http\Resources\AuthorCollection;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,7 +20,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('v1/authors', 'AuthorController', ['only' => ['index']]);
+//Route::apiResource('v1/authors', 'AuthorController', ['only' => ['index']]);
 // Route::group(['middleware' => 'auth:api'], function () {
 //     Route::apiResource('authors', 'AuthorController', ['only' => ['index']]);
 // });
+
+Route::get('/v1/authors/{id}', function ($id) {
+    return new AuthorResource(Author::find($id));
+});
+
+Route::get('/v1/authors', function () {
+    return new AuthorCollection(Author::paginate());
+});
